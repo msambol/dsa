257abe1064f59063539b5da65829ad16b670240c
Michael Sambol <sambol.michael@gmail.com>
2022-04-04 10:58:53 -0700

    Add bubble sort

---
+++ sort/bubble_sort.py
@@ -0,0 +1,23 @@
+def bubble_sort(list):
+    n = len(list)
+
+    for i in range(n-1):
+        for j in range(n-1):
+            if list[j] > list[j+1]: 
+                tmp = list[j]
+                list[j] = list[j+1]
+                list[j+1] = tmp
+
+    return list
+
+
+def main():
+    list = [99, 0, 5, 20, 123, 0, -1, 72, 21, 22, 13, 8, 7, 67, 29, 1, 2, 4]
+    bubble_sort(list)
+    print(list)
+
+    list = [3, 9, 2, 1]
+    bubble_sort(list)
+    print(list)
+
+main()
