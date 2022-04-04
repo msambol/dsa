e9b595953f9972a771d4f340dbcb1231281803cc
Michael Sambol <sambol.michael@gmail.com>
2022-04-04 11:10:20 -0700

    Add selection sort

---
+++ sort/selection_sort.py
@@ -0,0 +1,28 @@
+def selection_sort(list):
+    n = len(list)
+
+    for i in range(n-1): 
+        min = i
+
+        for j in range(i+1, n):
+            if list[j] < list[min]:
+                min = j
+
+        if min != i:
+            temp = list[i]
+            list[i] = list[min]
+            list[min] = temp
+
+    return list
+
+
+def main():
+    list = [99, 0, 5, 20, 123, 0, -1, 72, 21, 22, 13, 8, 7, 67, 29, 1, 2, 4]
+    selection_sort(list)
+    print(list)
+
+    list = [3, 9, 2, 1]
+    selection_sort(list)
+    print(list)
+
+main()
