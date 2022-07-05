17fa156c228ad238863aa3bbe1ddbcadf20fe3fc
Michael Sambol <sambol.michael@gmail.com>
2022-07-04 21:46:11 -0700

    Update heap.py

--- data_structures/heap.py
+++ data_structures/heap.py
@@ -12,8 +12,8 @@ def parent(i):
 
 
 def max_heapify(a, i):
-    l = 2*i
-    r = 2*i + 1
+    l = left(i)
+    r = right(i)
 
     largest = i 
 
@@ -24,17 +24,26 @@ def max_heapify(a, i):
         largest = r 
     
     if largest != i:
+        # swap elements
         a[i], a[largest] = a[largest], a[i]
         max_heapify(a, largest)
+
 
 def build_max_heap(a):
     for i in range(floor(len(a)/2), 0, -1):
         max_heapify(a, i)
 
+
 def main():
+    # root is at index 1
+    # it can be at index zero but see here: https://www.quora.com/Why-do-indexes-for-heaps-start-at-1
+    # and: https://stackoverflow.com/questions/22900388/why-in-a-heap-implemented-by-array-the-index-0-is-left-unused
+
     a = [None, 0, 5, 20, 6, 12, 65, 1, 4, 9, 3, 89, 22, 25, 28, 10]
     build_max_heap(a)
 
+    # print heap starting with the root at index 1
     print(f'Heap: {a[1:]}')
+
 
 main()
