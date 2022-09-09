83735ad7cae504e870ecb0b7199cde718f3ce2ac
Michael Sambol <sambol.michael@gmail.com>
2022-09-09 12:46:00 -0700

    Add note on BFS

--- search/breadth_first_search.py
+++ search/breadth_first_search.py
@@ -14,12 +14,15 @@ graph = {
 
 def bfs(graph, node):
     visited = []
+    # the video has queue as an array. I changed this to deque because popping the first element is O(1) instead of O(n).
+    # see this link for more: https://wiki.python.org/moin/TimeComplexity.
     queue = deque()
 
     visited.append(node)
     queue.append(node)
 
     while queue:
+        # popleft is O(1). for an array, pop(0) is O(n). hence the change to deque from array.
         s = queue.popleft()
         print(s, end = " ")
 
